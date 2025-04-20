c4136d23efb933de8bced542f36c88ab22ddd2eb
Felipe <felipebs10@gmail.com>
2025-04-20 19:33:47 -0300

    Programas de Estudo
    Começando a Estudar GO.

---
+++ 01Aula-Funcaomain/Primeiroscódigos/go.mod
@@ -0,0 +1,3 @@
+module github.com/felipebs10/curso-go
+
+go 1.24.2
---
+++ 01Aula-Funcaomain/Primeiroscódigos/main.go
@@ -0,0 +1,10 @@
+package main 
+
+//import "fmt"
+import (
+"github.com/felipebs10/curso-go/meet")
+
+func main () {
+	meet.SayHello()
+	meet.Say("Olá Felipe")
+}
