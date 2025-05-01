637d04b09d4a7b2a7f20f4fccd1374695df37f81
Felipe <felipebs10@gmail.com>
2025-05-01 11:33:47 -0300

    Primeiras aulas em go CRUD

---
+++ PrimeiroProjetoCRUD/go.mod
@@ -0,0 +1,5 @@
+module github.com/felipebs10/Estudando-GO/tree/main/PrimeiroProjetoCRUD
+
+go 1.24.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- PrimeiroProjetoCRUD/main.go
+++ PrimeiroProjetoCRUD/main.go
@@ -1,37 +1,18 @@
-package main 
+package main
 
-import ("fmt"
-		"time")
+import (
+	"fmt"
+	"log"
+	"os"
 
-func main(){
-	//Declarando um channel que vai ler um inteiro.
-	ch := make (chan int, 3) //esse 3 configura par que seja possível escrever 3 vezes, caso tenha uma quarta vez vai esperar liberar para escrever. 
+	"github.com/joho/godotenv"
+)
 
-	//Escrevendo a seta fica do lado direito (exemplo: ch <-) e se quer ler fica do lado esquerdo(exemplo: <-ch)
-	go func(){ 
-		//Exemplo de atribuição de valor simpes no channel
-		/*	ch <- 3
-		ch <- 2
-		ch <- 1*/ 
-
-		//Exemplo de atribuição de valor com for no channel
-		for i:= 0; i < 5; i++{
-			ch <- i
-		}
-		close(ch) //Ao abrir um channel, deve ser fechado para não causar o deadlock.
-		//Não apresentou esse print, 
-		fmt.Println("Escrita Finalizada")
-	}()
-
-	time.Sleep(time.Second * 1)
-	/*1º Exemplo */
-	/*<-ch //Retirando o valor mostrar que vai pegar o 3
-	<-ch
-	valor := <-ch
-	fmt.Println("Valor do canal:", valor) */
-	/* 2ª Exemplo com For*/ 
-	for valor := range ch {
-		fmt.Println("Leitura", valor)
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
+	fmt.Println(os.Getenv("TEST"))
 }
