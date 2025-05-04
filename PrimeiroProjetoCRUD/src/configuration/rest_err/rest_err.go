f0b87145aea55bfeb8af899c8ef8b5eb5b5fcc09
Felipe <felipebs10@gmail.com>
2025-05-04 12:06:37 -0300

    Criando tratamento de erros

---
+++ PrimeiroProjetoCRUD/src/configuration/rest_err/rest_err.go
@@ -0,0 +1,74 @@
+package rest_err
+
+import "net/http"
+
+type RestErr struct {
+	Message string   `json:"message"`
+	Err     string   `json:"error"`
+	Code    int      `json:"code"`  //Ele pede para deixar como int no vídeo, porque mais para frente ocorre um problema por causa do int64
+	Causes  []Causes `json:"causes` //Objeto cause
+}
+
+type Causes struct {
+	Field   string `json:"field`
+	Message string `json:"message`
+}
+
+func (r *RestErr) Error() string {
+	/*Feito essa função para retornar o erro, para evitar conflito com erros de bibliotecas*, nesse formato virá da interface.*/
+	return r.Message
+}
+
+/*Criando um construtor*/
+func NewRestErr(message, err string, code int, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+// Criando os métodos - Para validar as requests
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+
+func NewNotFoundError (message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err: 	 "not_found"
+		Code: 	 http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) * RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden"
+		Code:    http.StatusForbidden,
+	}
+}
+
---
+++ PrimeiroProjetoCRUD/src/controller/teste.go
@@ -0,0 +1,17 @@
+/* package main
+
+import "github.com/felipebs10/Estudando-GO/tree/main/PrimeiroProjetoCRUD/src/configuration/rest_err"
+*/
+//Com o rest_err.go tratando o erro ao invés de fzer no formato abaixo.
+/*
+func test(message string) error{
+	c.JSON
+}
+*/
+
+//Formato correto de fazer com o rest.err.go eXemplo porém não concluído.
+/*
+func test(message string) (err *rest_err.RestErr) {
+	err.Code
+}
+*/
