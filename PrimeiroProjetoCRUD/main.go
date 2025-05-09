4f4dd076de3d8384e58292907740aab67a4d68b8
Felipe <felipebs10@gmail.com>
2025-05-08 22:06:53 -0300

    Criando a primeira rota create user

--- PrimeiroProjetoCRUD/main.go
+++ PrimeiroProjetoCRUD/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/felipebs10/Estudando-GO/tree/main/PrimeiroProjetoCRUD/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env File")
 	}
 
-	fmt.Println(os.Getenv("TEST"))
+	router := gin.Default()
+	routes.InitRoutes(&router.RouterGroup) //Passando o ponteiro, o local onde está a armazenado o valor.
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+
+	/*A instrução router := gin.New() criará um novo roteador gin. Os
+	roteadores podem ser inicializados de duas maneiras, uma usando gin.New()
+	e a outra usando gin.Default().
+
+	A diferença é que gin.New() inicializa um roteador sem nenhum
+	middleware enquanto gin.Default() inicializa o roteador com logger
+	e middlewares de recovery.
+	*/
+
 }
--- PrimeiroProjetoCRUD/src/controller/createUser.go
+++ PrimeiroProjetoCRUD/src/controller/createUser.go
@@ -1,5 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/felipebs10/Estudando-GO/tree/main/PrimeiroProjetoCRUD/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+	err := rest_err.NewBadRequestError("Você chamnou a rota de forma errada")
+	c.JSON(err.Code, err) //Retornando o erro.
+}
