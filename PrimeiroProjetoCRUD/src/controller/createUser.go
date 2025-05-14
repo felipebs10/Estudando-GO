47ccfacf39f937759749b1f9b436814d98bd722f
Felipe <felipebs10@gmail.com>
2025-05-13 22:17:52 -0300

    Criando objetos em GO

--- PrimeiroProjetoCRUD/src/controller/createUser.go
+++ PrimeiroProjetoCRUD/src/controller/createUser.go
@@ -1,11 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/felipebs10/Estudando-GO/tree/main/PrimeiroProjetoCRUD/src/configuration/rest_err"
+	"github.com/felipebs10/Estudando-GO/tree/main/PrimeiroProjetoCRUD/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamnou a rota de forma errada")
-	c.JSON(err.Code, err) //Retornando o erro.
+	var UserRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&UserRequest); err != nil {
+		restErr := rest_err.NewBadRequestError( //Tratando o erro
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+
+		//Retorno do json
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(UserRequest)
 }
