79dd7c0e628334c05f582ce3e8a8cc00d96d68ab
Felipe <felipebs10@gmail.com>
2025-05-15 22:04:12 -0300

    Criando validações

--- PrimeiroProjetoCRUD/src/controller/model/request/user_request.go
+++ PrimeiroProjetoCRUD/src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"` // Uma variável que ocupa menos espaço
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,containsany=!@#$%*"`
+	Name     string `json:"name" binding:"required,min=4,max=100"`
+	Age      int8   `json:"age" binding:"required,min=1,max=140"` // Uma variável que ocupa menos espaço
 }
